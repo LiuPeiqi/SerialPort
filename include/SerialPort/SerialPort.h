074d5a5fa816b52798a0e3d5bf4084f1403877c6
LPQ <LPQ@LPQ-PC>
2017-06-28 20:43:19 +0800

    fix delay bug.

--- include/SerialPort/SerialPort.h
+++ include/SerialPort/SerialPort.h
@@ -69,55 +69,55 @@ namespace lpq {
 		public: BufferFullError(const std::string& msg) : std::runtime_error(msg) {}
 		};
 
-		SerialPort(const std::string& com,
+		SerialPort(const std::string& com_name,
 			unsigned int baud_rate = 9600,
 			StopBits stop_bits = StopBits::one,
 			Parity parity = Parity::none,
 			FlowControl flow_control = FlowControl::none,
 			unsigned int data_bits = 8) :
-			serial_port_lock(std::make_unique<std::mutex>()),
-			break_thread_flag(std::make_unique<bool>(false))
+			com(io_service, com_name),
+			receive_buffer(boost::circular_buffer<char>(1024)),
+			break_receive_flag(false)
 		{
-			std::lock_guard<std::mutex> construction_lock(*serial_port_lock);
-			prop = std::make_unique<Property>(com, baud_rate, stop_bits, parity, flow_control, data_bits);
-			read_thread = std::make_unique<std::thread>(
-				std::bind(
-					std::mem_fn(&SerialPort::Property::ReceiveThread), prop.get(), serial_port_lock.get(),break_thread_flag.get()));
+			std::lock_guard<std::mutex> construction_lock(serial_port_lock);
+			com.set_option(boost::asio::serial_port::baud_rate(baud_rate));
+			com.set_option(boost::asio::serial_port::stop_bits(stop_bits));
+			com.set_option(boost::asio::serial_port::parity(parity));
+			com.set_option(boost::asio::serial_port::flow_control(flow_control));
+			com.set_option(boost::asio::serial_port::character_size(data_bits));
+
+			std::thread read_thread(std::bind(&SerialPort::ReceiveThread, this));
+			read_thread.detach();
+			std::this_thread::sleep_for(timeout);
 		}
-		SerialPort(SerialPort&& right)
-		{
-			std::lock_guard<std::mutex>(*right.serial_port_lock);
-			prop = std::move(right.prop);
-			read_thread = std::move(right.read_thread);
-			break_thread_flag = std::move(right.break_thread_flag);
-			timeout = std::move(right.timeout);
-			serial_port_lock = std::move(right.serial_port_lock);
-		}
+
 		~SerialPort() {
-			if (prop != nullptr) {
-				std::lock_guard<std::mutex> sp_lock(*serial_port_lock);
-				*break_thread_flag = true;
-				read_thread->join();
-				prop->port.close();
-			}
+			std::lock_guard<std::mutex> sp_lock(serial_port_lock);
+			break_receive_flag = true;
+			com.close();
 		}
+
+		SerialPort(SerialPort&& right) = delete;
 		SerialPort(const SerialPort&) = delete;
 		SerialPort& operator=(const SerialPort&) = delete;
 
 		size_t Send(const std::string& msg) 
 		{
-			std::lock_guard<std::mutex> send_lock(*serial_port_lock);
+			std::lock_guard<std::mutex> send_lock(serial_port_lock);
 			return SendWithoutLock(msg);
 		}
 
 		std::string Receive(size_t byte_count)//return string.size() maybe not eq byte_count;
 		{
+			if (byte_count == 0) {
+				return std::string();
+			}
 			auto receive_complete = [=](BufIter begin, BufIter end) {
 				if (static_cast<size_t>(end - begin) >= byte_count) {
 					return begin + byte_count;
 				}
 				else {
-					return end;
+					return begin;
 				}
 			};
 			return std::move(ReceiveUntil(receive_complete));
@@ -129,7 +129,7 @@ namespace lpq {
 			auto search = [&](BufIter begin, BufIter end) {
 				auto iter = std::search(begin, end, termainal.cbegin(), termainal.cend());
 				if (iter == end) {
-					return end;
+					return begin;
 				}
 				iter += termainal.size();
 				return iter;
@@ -139,25 +139,33 @@ namespace lpq {
 
 		void CleanReceiveBuffer()
 		{
-			std::lock_guard<std::mutex> clean_lock(*serial_port_lock);
-			prop->buffer.erase_begin(prop->buffer.size());
+			std::lock_guard<std::mutex> clean_lock(serial_port_lock);
+			receive_buffer.erase_begin(receive_buffer.size());
 		}
 
-		std::string Demand(const std::string& cmd, const std::string& confirm_regex)
+		std::string Demand(const std::string& cmd, std::string confirm_regex, bool ignore_case = false)
 		{//if not confirm whith confirm_regex, only throw TimeoutError.
+			if ((confirm_regex.size() > 0) && confirm_regex[0] != '^') {
+				confirm_regex.insert(confirm_regex.begin(), '^');
+			}
 			{
-				std::lock_guard<std::mutex> clean_buffer_lock(*serial_port_lock);
-				prop->buffer.erase_begin(prop->buffer.size());
+				std::lock_guard<std::mutex> clean_buffer_lock(serial_port_lock);
+				receive_buffer.erase_begin(receive_buffer.size());
 				SendWithoutLock(cmd);
 			}//release lock
-			std::regex re_confirm(confirm_regex);
+			auto icase = std::regex::icase ^ std::regex::icase;
+			if (ignore_case) {
+				icase = std::regex::icase;
+			}
+			std::regex re_confirm(confirm_regex, icase);
+			
 			auto confirm = [&](BufIter begin, BufIter end) {
 				std::match_results<BufIter> result;
-				if (std::regex_match(begin, end, result, re_confirm)) {
+				if (std::regex_search(begin, end, result, re_confirm)) {
 					return begin + result[0].length();
 				}
 				else {
-					return end;
+					return begin;
 				}
 			};
 			try {
@@ -173,92 +181,72 @@ namespace lpq {
 		//using ConfirmCallBack = std::function<BufIter(BufIter buf_beg, BufIter buf_end)>;
 		//std::string Demand(const std::string& cmd, ConfirmCallBack confirm);
 
-		void ResponseTimeout(double timeout);
+		void ResponseTimeout(double timeout)//second
+		{
+			std::lock_guard<std::mutex> lock(serial_port_lock);
+			this->timeout = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::duration<double>(timeout));
+		}
 		double ResponseTimeout() const;
-		void BufferSize(size_t buffer_size);
-
-		size_t BufferSize() const { return prop->buffer_size; }
 
 		void Override(bool flag);
 		bool Overrider() const;
 		void Terminal(const std::string& t)
 		{
-			std::lock_guard<std::mutex> set_lock(*serial_port_lock);
-			this->prop->terminal = t;
+			std::lock_guard<std::mutex> set_lock(serial_port_lock);
+			terminal = t;
 		}
 
-		const std::string& Terminal()
+		const std::string& Terminal() const
 		{
-			std::lock_guard<std::mutex> set_lock(*serial_port_lock);
-			return this->prop->terminal;
+			return terminal;
 		}
 	private:
-		struct Property{
-			boost::asio::io_service io;
-			boost::asio::serial_port port;
-			std::string port_name;
-			std::string terminal;
-			boost::circular_buffer<char> buffer;
-			size_t buffer_size = 1024;
-
-			Property(const std::string& com, unsigned int baud_rate, StopBits stop_bits,
-				Parity parity, FlowControl flow_control, unsigned int data_bits):
-				io(), port(io, com), port_name(com)
+			void ReceiveThread()
 			{
-				port.set_option(boost::asio::serial_port::baud_rate(baud_rate));
-				port.set_option(boost::asio::serial_port::stop_bits(stop_bits));
-				port.set_option(boost::asio::serial_port::parity(parity));
-				port.set_option(boost::asio::serial_port::flow_control(flow_control));
-				port.set_option(boost::asio::serial_port::character_size(data_bits));
-				
-				buffer = boost::circular_buffer<char>(buffer_size);
-			}
-
-			void ReceiveThread(std::mutex* write_lock, volatile bool* break_flag)
-			{
-				volatile size_t async_receive_bytes = 0;
-				volatile bool async_read_complete = true;
-				auto async_read_handler = [&](const boost::system::error_code& ec, size_t bytes)->void {
-					if (ec) {
-						//log and exit thread
-						;
-					}
-					async_receive_bytes = bytes;
-					async_read_complete = true;
-				};
 				const static size_t READ_SIZE = 128;
 				char copy_buf[READ_SIZE];
-				while (!*break_flag) {
-					if (async_read_complete) {
-						std::lock_guard<std::mutex> sp_guard(*write_lock);
-						std::copy(copy_buf, copy_buf + async_receive_bytes, std::back_inserter(buffer));
-						async_read_complete = false;
-						async_receive_bytes = 0;
-						port.async_read_some(boost::asio::buffer(copy_buf, READ_SIZE), async_read_handler);
-						io.run();
+				while (!break_receive_flag) {
+					boost::system::error_code ec;
+					auto receive_bytes = com.read_some(boost::asio::buffer(copy_buf, READ_SIZE), ec);
+					if (ec) {
+						if (ec.value() == 995) {
+							//exit
+						}
+						else {
+							//log error
+						}
+						break;
+					}
+					if(receive_bytes != 0){
+						std::lock_guard<std::mutex> lock(serial_port_lock);
+						std::copy(copy_buf, copy_buf + receive_bytes, std::back_inserter(receive_buffer));
+						receive_bytes = 0;
 					}//release lock
 					std::this_thread::sleep_for(INTERVAL);
 				}
-				port.cancel();
+				break_receive_flag = true;
 				return;
 			}
-		};
 
-
-		std::unique_ptr<std::mutex> serial_port_lock;
-		std::unique_ptr<Property> prop;
-		std::unique_ptr<std::thread> read_thread;
-		std::unique_ptr<volatile bool> break_thread_flag;
-		std::chrono::milliseconds timeout = std::chrono::milliseconds(20000);
-		static constexpr std::chrono::milliseconds INTERVAL = std::chrono::milliseconds(20);
+		std::mutex serial_port_lock;
+		std::chrono::milliseconds timeout = std::chrono::milliseconds(20);
+		boost::asio::io_service io_service;
+		boost::asio::serial_port com;
+		boost::circular_buffer<char> receive_buffer;
+		std::string terminal;
+		volatile bool break_receive_flag;
+		static constexpr std::chrono::milliseconds INTERVAL = std::chrono::milliseconds(1);
 
 		size_t SendWithoutLock(const std::string& msg)
 		{
-			auto byte = prop->port.write_some(boost::asio::buffer(msg));
+			if (break_receive_flag) {
+				throw SendError("SerialPort has closed!");
+			}
+			auto byte = com.write_some(boost::asio::buffer(msg));
 			auto check_size = msg.size();
-			if (!prop->terminal.empty()) {
-				check_size += prop->terminal.size();
-				byte += prop->port.write_some(boost::asio::buffer(prop->terminal));
+			if (!terminal.empty()) {
+				check_size += terminal.size();
+				byte += com.write_some(boost::asio::buffer(terminal));
 			}
 			if (byte != check_size) {
 				throw SendError("Send byte != cmd.size:\"" + msg + "\"" + std::to_string(byte) + "Bytes!");
@@ -270,64 +258,39 @@ namespace lpq {
 
 		std::string ReceiveUntil(std::function<BufIter(BufIter begin, BufIter end)> predicate)
 		{
-			auto start = std::chrono::system_clock::now();
+			if (break_receive_flag) {
+				throw ResponseError("SerialPort has closed!");
+			}
 			size_t last_size = 0;
 			std::string buf;
+			auto start = std::chrono::system_clock::now();
+			size_t count = 0;
 			while (true) {
 				{
-					std::lock_guard<std::mutex> read_lock(*serial_port_lock);
-					auto end_iter = predicate(prop->buffer.begin(), prop->buffer.end());
-					if (end_iter != prop->buffer.end()) {
-						buf.assign(prop->buffer.begin(), end_iter);
-						prop->buffer.erase(prop->buffer.begin(), end_iter);
+					std::lock_guard<std::mutex> lock(serial_port_lock);
+					if (0 == count++) {
+						start = std::chrono::system_clock::now();
+					}
+					auto end_iter = predicate(receive_buffer.begin(), receive_buffer.end());
+					if (end_iter != receive_buffer.begin()) {
+						buf.assign(receive_buffer.begin(), end_iter);
+						receive_buffer.erase(receive_buffer.begin(), end_iter);
 						return std::move(buf);
 					}
 					auto step = std::chrono::system_clock::now();
 					auto elapsed_time = std::chrono::duration_cast<std::chrono::microseconds>(step - start);
-					if (elapsed_time > timeout) {
+					if (last_size == receive_buffer.size() && elapsed_time > timeout) {
+						receive_buffer.erase_begin(receive_buffer.size());
 						throw TimeoutError("ReceiveUntil have no more new bytes in " + std::to_string(timeout.count()) + "ms!");
 					}
-					if (last_size != prop->buffer.size()) {
-						last_size = prop->buffer.size();
+					if (last_size != receive_buffer.size()) {
+						last_size = receive_buffer.size();
 						start = step;
 					}
 				}// release lock
 				std::this_thread::sleep_for(INTERVAL);
 			}
 			return buf;// never run here;
-		}
-
-		void ReceiveThread()
-		{
-			// store local pointe to access resources when move construction occurred.
-			std::mutex* const sp_lock = this->serial_port_lock.get();
-			Property* const prop = this->prop.get();
-			volatile bool * const break_flag = this->break_thread_flag.get();
-			volatile size_t async_receive_bytes = 0;
-			volatile bool async_read_complete = true;
-			auto async_read_handler = [&](const boost::system::error_code& ec, size_t bytes)->void {
-				if (ec) {
-					//log and exit thread
-					;
-				}
-				async_receive_bytes = bytes;
-				async_read_complete = true;
-				prop->io.run();
-			};
-			const static size_t READ_SIZE = 128;
-			char copy_buf[READ_SIZE];
-			while (!*break_flag) {
-				if(async_read_complete){
-					std::lock_guard<std::mutex> sp_guard(*sp_lock);
-					std::copy(copy_buf, copy_buf + async_receive_bytes, std::back_inserter(prop->buffer));
-					async_read_complete = false;
-					async_receive_bytes = 0;
-					prop->port.async_read_some(boost::asio::buffer(copy_buf, READ_SIZE), async_read_handler);
-				}//release lock
-				std::this_thread::sleep_for(INTERVAL);
-			}
-			prop->port.cancel();
-			return;
 		}
 	};
 }
--- src/SerialPort/test_main.cpp
+++ src/SerialPort/test_main.cpp
@@ -1,16 +1,108 @@
 #include "SerialPort/SerialPort.h"
 #include <chrono>
 #include <iostream>
+#include <sstream>
+#include <thread>
+#include <vector>
 using namespace std;
 int main()
 {
 	auto coms = lpq::ListSerialPorts();
+	vector<size_t> slots928;
+	vector<size_t> slots911;
 	try {
-		lpq::SerialPort serial_port(coms[1]);
+		lpq::SerialPort serial_port(coms[1], 115200);
+		serial_port.ResponseTimeout(0.1);
 		serial_port.Terminal("\r\n");
-		serial_port.Send("*idn?");
-		while (true) {
-			cout << serial_port.ReceiveUntil("\r\n") << endl;
+		serial_port.Send("Exit");
+		serial_port.Send("*rst");
+		for (size_t i = 1; i < 9; ++i) {
+			stringstream sst;
+			sst << "GETN? " << i << ", 1024";
+			serial_port.Send(sst.str());
+			sst.str("");
+			this_thread::sleep_for(1s);
+			serial_port.CleanReceiveBuffer();
+			sst << "SNDT " << i << ", '*rst'" << endl;
+			serial_port.Send(sst.str());
+			sst.str("");
+			sst << "SNDT " << i << ", '*idn?'" << endl;
+			serial_port.Send(sst.str());
+			sst.str("");
+			this_thread::sleep_for(1s);
+			sst << "GETN?  " << i << ", 100" << endl;
+			serial_port.Send(sst.str());
+			auto idn = serial_port.ReceiveUntil("\r\n");
+			serial_port.CleanReceiveBuffer();
+			if (idn.size() > 7) {
+				cout << i << ": " << idn;
+			}
+			if (idn.find("928") != string::npos) {
+				slots928.push_back(i);
+				/*sst.str("");
+				sst << "SNDT " << i << ",\"BAUD 156250\"";
+				serial_port.Send(sst.str());
+				this_thread::sleep_for(1s);
+				sst.str("");
+				sst << "BAUD " << i << ",156250";
+				serial_port.Send(sst.str());
+				serial_port.Send("*idn?");
+				auto res = serial_port.ReceiveUntil("\r\n");
+				cout << res;
+				sst.str("");
+				sst << "BAUD? " << i;
+				serial_port.Send(sst.str());
+				res = serial_port.ReceiveUntil("\r\n");
+				cout << res;
+				this_thread::sleep_for(1s);
+				sst.str("");
+				sst << "CONN " << i << ",'Exit'";
+				serial_port.Send(sst.str());
+				serial_port.Send("*idn?");
+				res = serial_port.ReceiveUntil("\r\n");
+				cout << res;
+				serial_port.Send("Exit");*/
+				sst.str("");
+				sst << "SNDT " << i << ", '*idn?'";
+				serial_port.Send(sst.str());
+				sst.str("");
+				this_thread::sleep_for(1s);
+				sst << "GETN? " << i << ",100";
+				serial_port.Send(sst.str());
+				idn = serial_port.Receive(5);
+				size_t bytes = stoi(string(idn.begin() + 2, idn.end()));
+				idn = serial_port.Receive(bytes);
+				cout << idn;
+				serial_port.CleanReceiveBuffer();
+			}
+			else if (idn.find("911") != string::npos) {
+				slots911.push_back(i);
+			}
+		}
+		serial_port.Demand("*idn?", "Stanford_Research_Systems,\\s*SIM900,\\s*s/n\\d+,\\s*ver\\d+(\\.\\d+)?\\r\\n", true);
+		for (size_t i = 0; i < 1000; ++i) {
+			auto start = chrono::system_clock::now();
+			for (const auto& slot : slots928) {
+				stringstream sst;
+				sst << "CONN " << slot << ", \"Exit\"";
+				serial_port.Send(sst.str());
+				serial_port.Demand("*idn?", "Stanford_Research_Systems,\\s*SIM928,\\s*s/n\\d+,\\s*ver\\d+(\\.\\d+)?\\r\\n", true);
+				serial_port.Demand("volt 1\r\nvolt?", "[\\+-]?\\d+(\\.\\d+)?\\r\\n");
+				serial_port.Demand("opon\r\nexon?", "1\\r\\n");
+				serial_port.Demand("volt 0\r\nvolt?", "[\\+-]?\\d+(\\.\\d+)?\\r\\n");
+				serial_port.Demand("opof\r\nexon?", "0\\r\\n");
+				serial_port.Send("Exit");
+			}
+			for (const auto& slot : slots911) {
+				stringstream sst;
+				sst << "CONN " << slot << ", \"Exit\"";
+				serial_port.Send(sst.str());
+				serial_port.Demand("*idn?", "stanford_research_systems,\\s*SIM911,\\s*s/n\\d+,\\s*ver\\d+(\\.\\d+)?\\r\\n", true);
+				serial_port.Send("Exit");
+				serial_port.Demand("*idn?", "Stanford_Research_Systems,\\s*SIM900,\\s*s/n\\d+,\\s*ver\\d+(\\.\\d+)?\\r\\n", true);
+			}
+			auto finish = chrono::system_clock::now();
+			cout << chrono::duration_cast<chrono::milliseconds>(finish - start).count() << "ms" << endl;
 		}
 	}
 	catch (exception& e) {
